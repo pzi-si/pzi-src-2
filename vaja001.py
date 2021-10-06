f932306d67a4a9ae7dc09870c9576d2f0dd1276b
Robert Klinc <robert@klinc.net>
2021-10-06 12:04:19 +0200

    test

---
+++ vaja001.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Program, ki zmnoži podani števili. """
+
+# Podamo številki, ki ju bomo zmnožili
+mnozenec = 2
+mnozitelj = 4
+
+# zmnožimo števili
+zmnozek = mnozenec * mnozitelj
+
+# izpišemo vsoto
+print(zmnozek)
+#print(f"Rezultat: {mnozenec} * {mnozitelj} = {zmnozek}")
