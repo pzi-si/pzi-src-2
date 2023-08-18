9df533d98dce5cf3b8ec091967b57fced223d8ee
Robert Klinc <robert@klinc.net>
2023-08-18 12:02:56 +0200

    dodana naloga 11

---
+++ naloga011b.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Napiši program, ki izpiše prvih _X_ členov Fibonaccijevega zaporedja. Število _X_ na poziv vnese uporabnik.
+"""
+
+# Uporabnika pozovemo, da vpiše število, koliko členov Fibonaccijevega zaporedja naj prikažemo
+stevilo = input("Vpišite, koliko členov Fibonaccijevega zaporedja vas zanima: ")
+
+# preverimo, ali je podano število tipa 'integer'
+try:
+    stevilo = int(stevilo)
+except:
+    print("Niste podali celega števila!")
+    quit()
+
+#ker moramo vedno poznati dve števili zaporedja, uvedemo dve novi neznanki
+stevilo_ena = stevilo_dva = 1
+# za vsa števila od 1 do izbranega števila + 1 izračunamo in izpišemo
+for i in range(1,stevilo+1):
+    print(stevilo_ena)
+    stevilo_ena, stevilo_dva = stevilo_dva,stevilo_ena+stevilo_dva
